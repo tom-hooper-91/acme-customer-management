e65f4a21671723131120728cca6a708622b5f2a9
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-24 12:28:44 +1200

    create OrderItem class

---
+++ ACM.BL/OrderItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ACM.BL
+{
+    public class OrderItem
+    {
+        //constructors
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int orderItemId)
+        {
+            OrderItemId = orderItemId;
+        }
+
+        //properties
+
+        public int OrderItemId{
+            get;
+            private set;
+        }
+        public Product product { get; set; }
+        public int PurchasePrice { get; set; }
+
+        //methods
+
+        public bool Validate()
+        {
+            //code to validate
+            return true;
+        }
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            //code to locate OrderItem
+            return new OrderItem();
+        }
+
+        public List<OrderItem> Retrieve()
+        {
+            //code to return all OrderItems
+            return new List<OrderItem>();
+        }
+
+        public bool Save()
+        {
+            //code to save current OrderItem
+            return true;
+        }
+    }
+}
