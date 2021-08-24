91d4425f06fdddb625a6a90bd47f31034f73a006
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-24 16:41:57 +1200

    write test for CustomerRepository

---
+++ ACM.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ACM.BL;
+using Xunit;
+
+namespace ACM.BLTest
+{
+    public class CustomerRepositoryTest
+    {
+        [Fact]
+        public void RetrieveValid()
+        {
+            //arrange
+            var customerRepository = new CustomerRepository();
+
+            var expected = new Customer(1)
+            {
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                EmailAddress = "fbaggins@hobbiton.me"
+            };
+
+            //act
+            var actual = customerRepository.Retrieve(1);
+
+            //assert
+            Assert.Equal(expected.FirstName, actual.FirstName);
+            Assert.Equal(expected.LastName, actual.LastName);
+            Assert.Equal(expected.EmailAddress, actual.EmailAddress);
+            Assert.Equal(expected.CustomerId, actual.CustomerId);
+        }
+    }
+}
