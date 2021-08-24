5857c01f9c5ba442a083e14ad8cd3b8842c8af75
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-24 17:05:07 +1200

    write tests for Repository classes

---
+++ ACM.BLTest/OrderRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ACM.BL;
+using Xunit;
+
+namespace ACM.BLTest
+{
+    public class OrderRepositoryTest
+    {
+        [Fact]
+        public void RetrieveValid()
+        {
+            //arrange
+            var orderRepository = new OrderRepository();
+
+            var expected = new Order(10)
+            {
+                OrderDate = new DateTimeOffset(DateTime.Now.Year, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0))
+            };
+
+            //act
+            var actual = orderRepository.Retrieve(10);
+
+            //assert
+            Assert.Equal(expected.OrderDate, actual.OrderDate);
+        }
+    }
+}
---
+++ ACM.BLTest/ProductRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ACM.BL;
+using Xunit;
+
+namespace ACM.BLTest
+{
+    public class ProductRepositoryTest
+    {
+        [Fact]
+        public void RetrieveValid()
+        {
+            //arrange
+            var productRepository = new ProductRepository();
+
+            var expected = new Product(2)
+            {
+                ProductName = "Sunflowers",
+                Description = "Assorted size bunch of 4 yellow Sunflowers",
+                CurrentPrice = 16.96M
+        };
+
+            //act
+            var actual = productRepository.Retrieve(2);
+
+            //assert
+            Assert.Equal(expected.ProductName, actual.ProductName);
+            Assert.Equal(expected.Description, actual.Description);
+            Assert.Equal(expected.CurrentPrice, actual.CurrentPrice);
+        }
+    }
+}
