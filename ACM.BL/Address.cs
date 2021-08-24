ac126e3cec1f6f83e1634533fc3fbcd463a10fd6
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-24 16:08:59 +1200

    create Address class

---
+++ ACM.BL/Address.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ACM.BL
+{
+    public class Address
+    {
+        //constructors
+        public Address()
+        {
+        }
+
+        public Address(int addressId)
+        {
+            AddressId = addressId;
+        }
+
+        //properties
+        public int AddressId{
+            get;
+            private set;
+        }
+        public int AddressType { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+        //methods
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (PostalCode == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
