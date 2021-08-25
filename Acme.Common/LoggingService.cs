5ae1a29c3571e98665e00b4b83b3c573ac46344c
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-25 17:05:33 +1200

    write test for LoggingService

---
+++ Acme.Common/LoggingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Acme.Common
+{
+    public static class LoggingService
+    {
+        public static void WriteToFile(List<ILoggable> itemsToLog)
+        {
+            foreach (var item in itemsToLog)
+            {
+                Console.Write(item.Log());
+            }
+        }
+    }
+}
---
+++ Acme.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using ACM.BL;
+using System.Collections.Generic;
+using Acme.Common;
+using Xunit;
+
+namespace Acme.CommonTest
+{
+    public class LoggingServiceTest
+    {
+        [Fact]
+        public void WriteToFileTest()
+        {
+            //arrange
+            var changedItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                EmailAddress = "fbaggins@hobbiton.me",
+                AddressList = null
+            };
+
+            changedItems.Add(customer);
+
+            var product = new Product(2)
+            {
+                ProductName = "Rake",
+                Description = "A garden rake with a steel head",
+                CurrentPrice = 6M
+            };
+
+            changedItems.Add(product);
+
+            //act
+            LoggingService.WriteToFile(changedItems);
+
+            //assert
+            //Nothing to assert
+        }
+    }
+}
