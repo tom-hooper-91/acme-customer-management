b89f15002754a351e959ee56ce0680583ed602e2
Ben Amor <ben@adminis.co.nz>
2021-08-26 13:21:34 +1200

    Made a few mainly stylistic changes to a couple of tests, as an example

--- ACM.BLTest/CustomerTest.cs
+++ ACM.BLTest/CustomerTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ACM.BL;
+﻿using ACM.BL;
 using Xunit;
 
 namespace ACM.BLTest
@@ -7,34 +6,34 @@ namespace ACM.BLTest
     public class CustomerTest
     {
         [Fact]
-        public void FullNameTestValid()
+        public void First_name_and_last_name_are_concatenated_in_reverse_order_separated_by_a_comma()
         {
             //arrange
-            Customer customer = new Customer
+            var customer = new Customer
             {
                 FirstName = "Bilbo",
                 LastName = "Baggins"
             };
 
-            string expected = "Baggins, Bilbo";
+            var expected = "Baggins, Bilbo";
             //act
-            string actual = customer.FullName;
+            var actual = customer.FullName;
             //assert
             Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void FullNameFirstNameEmpty()
+        public void If_there_is_no_first_name_only_the_last_name_is_returned_in_FullName()
         {
             //arrange
-            Customer customer = new Customer
+            var customer = new Customer
             {
                 LastName = "Baggins"
             };
 
-            string expected = "Baggins";
+            var expected = "Baggins";
             //act
-            string actual = customer.FullName;
+            var actual = customer.FullName;
             //assert
             Assert.Equal(expected, actual);
         }
@@ -64,7 +63,7 @@ namespace ACM.BLTest
             Customer.InstanceCount++;
 
             var c2 = new Customer();
-            c1.FirstName = "Frodo";
+            c1.FirstName = "Frodo"; // BA Should this be c2.FirstName? Actually, if this is a test that the InstanceCount gets incremented correctly, do we need to be doing anything with names in this test?
             Customer.InstanceCount++;
 
             var c3 = new Customer();
